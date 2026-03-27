6534c81425f0a0f9b41d4c9fb80e1dc7893e740e
Adam Bermingham <adambermingham1@gmail.com>
2026-03-27 10:39:56 +0000

    Set up instrument delcaration and note playing function

--- node_2d.gd
+++ node_2d.gd
@@ -6,9 +6,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_instrument(0, 2)
+	
+	play_note(60)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_instrument(channel, instrument):
+	var midi_event = InputEventMIDI.new()
+	midi_event.channel = 0
+	midi_event.message = MIDI_MESSAGE_PROGRAM_CHANGE
+	midi_event.instrument = instrument
+	$MidiPlayer.receive_raw_midi_message(midi_event)
+
+func play_note(note):
+	var m = InputEventMIDI.new()
+	m.message = MIDI_MESSAGE_NOTE_ON
+	m.pitch = note
+	m.velocity = 100
+	m.channel = 0		
+	$MidiPlayer.receive_raw_midi_message(m)	
