59569242a2d6da24d184be05ac9e78f7a8b58619
Adam Bermingham <adambermingham1@gmail.com>
2026-04-16 11:11:44 +0100

    Refractered and tidied up code

--- quiz.gd
+++ quiz.gd
@@ -18,50 +18,48 @@ var pattern = []
 var pattern_length = []
 var response = []
 
+
+
+
+
+# Setup Functions
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
 	set_instrument(0,2)
 
-
+# function to reset starts for when running the quiz
+func start():
+	started = true
+	pattern = []
+	index_position = 0
+	response = []
+	score = 0
+	$Score.text = str('Score: ', score)
+	playing()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if $Exit.button_pressed:
 		get_tree().change_scene_to_file("res://main_menu.tscn")
 
 	if $Easy.button_pressed and not started:
-		started = true
-		pattern = []
-		index_position = 0
-		response = []
 		pattern_length = 5
-		score = 0
 		$Gamemode.text = 'Gamemode: Easy'
-		$Score.text = str('Score: ', score)
-		playing()
-	
+		start()
+
 	if $Medium.button_pressed and not started:
-		started = true
-		pattern = []
-		index_position = 0
-		response = []
 		pattern_length = 10
-		score = 0
 		$Gamemode.text = 'Gamemode: Medium'
-		$Score.text = str('Score: ', score)
-		playing()
+		start()
 	
 	if $Hard.button_pressed and not started:
-		started = true
-		pattern = []
-		index_position = 0
-		response = []
 		pattern_length = 15
-		score = 0
 		$Gamemode.text = 'Gamemode: Hard'
-		$Score.text = str('Score: ', score)
-		playing()
+		start()
 
+#function to setup instrument
 func set_instrument(channel, instrument):
 	var midi_event = InputEventMIDI.new()
 	midi_event.channel = 0
@@ -131,6 +129,10 @@ func _draw() -> void:
 
 
 
+
+
+# Music / Note Playing Functions
+
 func playing():
 	for x in pattern_length:
 		var index = randi_range(0,15)
@@ -146,7 +148,7 @@ func playing():
 	started = false
 	responding = true
 
-
+# play note function
 func play_note(pitch):
 	var m = InputEventMIDI.new()
 	m.message = MIDI_MESSAGE_NOTE_ON
@@ -167,7 +169,59 @@ func play_note_off(pitch):
 	white_keys = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	black_keys = [0, 0, 0, 0, 0, 0]
 	queue_redraw()
-	
+
+# code brought over from create_music.gd to help with the recalling function
+# function to take inputted key and play the appropiate music and display correctly
+func _input(event):
+	var note = 0
+	if started:
+		return
+
+	if event is InputEventKey:
+		if event.is_pressed() and not event.is_echo():
+			note = key_to_note(event.keycode)
+			if note != 0:
+				active_key(note)
+				play_note(note)
+				
+				if responding:
+					response.append(note)
+					recalling(response)
+		
+		elif event.is_released():
+			play_note_off(note)
+
+
+
+
+
+
+
+# Game Functions
+# function to manage how correct a guess and adjust
+func recalling(guess):
+	if guess[index_position] == pattern[index_position]:
+		score += 1
+	elif abs(guess[index_position] - pattern[index_position]) == 1:
+		score += 0.5
+		incorrect_key(pattern[index_position])
+	elif abs(guess[index_position] - pattern[index_position]) == 2:
+		score += 0.25
+		incorrect_key(pattern[index_position])
+	else:
+		incorrect_key(pattern[index_position])
+	index_position += 1
+	$Score.text = str('Score: ', score)
+	if index_position == pattern_length:
+		responding = false
+
+
+
+
+
+
+# Searching Functions
+
 # function to find key which is playing played and mark it as active
 func active_key(key):
 	match key:
@@ -188,6 +242,7 @@ func active_key(key):
 		66: black_keys[5] = 1
 		67: white_keys[9] = 1
 
+# function to find the index of what is the correct key and mark it
 func incorrect_key(key):
 	match key:
 		52: white_keys[0] = 2
@@ -207,90 +262,24 @@ func incorrect_key(key):
 		66: black_keys[5] = 2
 		67: white_keys[9] = 2
 
-# code brought over from create_music.gd to help with the recalling function
-func _input(event):
-	var note = 0
-	if started:
-		return
 
-	if event is InputEventKey:
-		if event.is_pressed() and not event.is_echo():
-			note = key_to_note(event.keycode)
-			if note != 0:
-				play_note(note)
-				
-				if responding:
-					response.append(note)
-					recalling(response)
-		
-		elif event.is_released():
-			play_note_off(note)
-			
+# function to find what note is linked to the keyboard key
 func key_to_note(key):
 	match key:
-		KEY_A: 
-			white_keys[0] = 1
-			return 52
-		KEY_S: 
-			white_keys[1] = 1
-			return 53
-		KEY_E: 
-			black_keys[0] = 1
-			return 54
-		KEY_D: 
-			white_keys[2] = 1
-			return 55
-		KEY_R: 
-			black_keys[1] = 1
-			return 56
-		KEY_F: 
-			white_keys[3] = 1
-			return 57
-		KEY_T: 
-			black_keys[2] = 1
-			return 58
-		KEY_G: 
-			white_keys[4] = 1
-			return 59
-		KEY_H: 
-			white_keys[5] = 1
-			return 60
-		KEY_U: 
-			black_keys[3] = 1
-			return 61
-		KEY_J: 
-			white_keys[6] = 1
-			return 62
-		KEY_I: 
-			black_keys[4] = 1
-			return 63
-		KEY_K: 
-			white_keys[7] = 1
-			return 64
-		KEY_L: 
-			white_keys[8] = 1
-			return 65
-		KEY_P: 
-			black_keys[5] = 1
-			return 66
-		KEY_SEMICOLON: 
-			white_keys[9] = 1
-			return 67
+		KEY_A: return 52
+		KEY_S: return 53
+		KEY_E: return 54
+		KEY_D: return 55
+		KEY_R: return 56
+		KEY_F: return 57
+		KEY_T: return 58
+		KEY_G: return 59
+		KEY_H: return 60
+		KEY_U: return 61
+		KEY_J: return 62
+		KEY_I: return 63
+		KEY_K: return 64
+		KEY_L: return 65
+		KEY_P: return 66
+		KEY_SEMICOLON: return 67
 		_: return 0
-
-
-func recalling(guess):
-	if guess[index_position] == pattern[index_position]:
-		score += 1
-	elif abs(guess[index_position] - pattern[index_position]) == 1:
-		score += 0.5
-		incorrect_key(pattern[index_position])
-	elif abs(guess[index_position] - pattern[index_position]) == 2:
-		score += 0.25
-		incorrect_key(pattern[index_position])
-	else:
-		incorrect_key(pattern[index_position])
-	index_position += 1
-	$Score.text = str('Score: ', score)
-	if index_position == pattern_length:
-		responding = false
